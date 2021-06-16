1619794086e53035b47c3e0b675421a7a645384c
mmskv <max.mishuk2014@gmail.com>
2021-06-16 21:34:50 +0300

    Add socket race condition test

--- test/socket.cpp
+++ test/socket.cpp
@@ -4,6 +4,8 @@
 #include <boost/test/unit_test.hpp>
 #include <boost/test/unit_test_log.hpp>
 #include <boost/test/unit_test_suite.hpp>
+#include <future>
+#include <mutex>
 #include <random>
 #include <string>
 #include <thread>
@@ -12,6 +14,7 @@
 #define MAX_NUM 10000000
 #define SMALL_MAX_NUM 38000
 
+// Overloads for boost::test to compare pixmaps
 bool
 operator== (const dexpo_pixmap d1, const dexpo_pixmap d2)
 {
@@ -42,10 +45,11 @@ BOOST_AUTO_TEST_CASE (send_vecotrs)
   dexpo_pixmap d3{ 3, s_rnd (gen), s_rnd (gen), m_rnd (gen), "VGA-1" };
 
   std::vector<dexpo_pixmap> v = { d0, d1, d2, d3 };
+  std::mutex v_lock;
 
   auto daemon = dexpo_socket ();
   std::thread daemon_thread (&dexpo_socket::send_pixmaps_on_event, &daemon,
-                             std::ref (v)); // Passing by reference
+                             std::ref (v), std::ref (v_lock));
   daemon_thread.detach ();
   BOOST_TEST_MESSAGE ("Started thread");
 
@@ -61,4 +65,50 @@ BOOST_AUTO_TEST_CASE (send_vecotrs)
     {
       BOOST_CHECK_EQUAL (v_recv[i], v[i]);
     }
+}
+
+/**
+ * If locks don't work, sent and received vectors will differ
+ */
+BOOST_AUTO_TEST_CASE (socket_race_condidion)
+{
+  // Initialize test desktop structs
+  dexpo_pixmap d0{ 0, s_rnd (gen), s_rnd (gen), m_rnd (gen), "HDMI-0" };
+  dexpo_pixmap d1{ 1, s_rnd (gen), s_rnd (gen), m_rnd (gen), "VDI-1" };
+  dexpo_pixmap d2{ 2, s_rnd (gen), s_rnd (gen), m_rnd (gen), "DP-0" };
+  dexpo_pixmap d3{ 3, s_rnd (gen), s_rnd (gen), m_rnd (gen), "VGA-1" };
+
+  std::vector<dexpo_pixmap> v = { d0, d1, d2, d3 };
+  std::mutex v_lock;
+
+  auto daemon = dexpo_socket ();
+  std::thread daemon_thread (&dexpo_socket::send_pixmaps_on_event, &daemon,
+                             std::ref (v), std::ref (v_lock));
+  daemon_thread.detach ();
+  BOOST_TEST_MESSAGE ("Started thread");
+
+  auto client = dexpo_socket ();
+  BOOST_TEST_MESSAGE ("Created client");
+
+  v_lock.lock ();
+  BOOST_TEST_MESSAGE ("Locked vector");
+
+  auto v_recv_future = std::async (&dexpo_socket::get_pixmaps, &client);
+  BOOST_TEST_MESSAGE ("Asked for pixmaps");
+
+  sleep (1);
+
+  v.erase (v.begin ());
+  BOOST_TEST_MESSAGE ("Removed element");
+
+  v_lock.unlock ();
+  BOOST_TEST_MESSAGE ("Unlocked vector");
+
+  auto v_recv = v_recv_future.get ();
+
+  daemon.running = false;
+
+  BOOST_TEST_MESSAGE ("Vector length: " << v.size ());
+
+  BOOST_CHECK (v_recv[0] == v[0]);
 }
