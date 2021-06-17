88deb94b66e2538174dec85c053220e72897e2d8
mangalinor <gssharikov@edu.hse.ru>
2021-06-17 03:32:38 +0300

    GUI for debug

--- src/config.hpp
+++ src/config.hpp
@@ -4,3 +4,5 @@ const int k_dexpo_width = 500;
 // Displayed screenshot height.
 // Will be calculated based on width and screen ratio if set to zero
 const int k_dexpo_height = 0;
+// Displayed interval between screenshots
+const int k_dexpo_padding = 10;
